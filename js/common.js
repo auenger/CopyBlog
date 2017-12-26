82f720b34a22cd1406189d8e46649a5bef8bf849
yzw40459 <yzw40459@ly.com>
2017-12-26 17:43:43 +0800

    --IMCODERS

--- js/common.js
+++ js/common.js
@@ -2,3 +2,17 @@ var reallyDocumentTitle = document.title
 document.addEventListener("visibilitychange", function() {
 	document.title = document.hidden ? "你妹的，死回来！" : reallyDocumentTitle;
 });
+window.onscroll=function(){
+	if($(window).scrollTop() > 80) {
+		$('.navbar-default').addClass('on');
+		$('.navbar-brand').addClass('is');
+		$('.navBar').addClass('up');
+	} else {
+		$('.navbar-default').removeClass('on');
+		$('.navbar-brand').removeClass('is');
+		$('.navBar').removeClass('up');
+	}
+}
+$(document).ready(function(){
+	$(".yx-rotaion").yx_rotaion({auto:true});
+});
