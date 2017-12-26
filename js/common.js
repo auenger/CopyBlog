b225df7c7e599046f5041b271d369da93d99b145
yzw <yzw40459@ly.com>
2017-12-26 23:35:41 +0800

    --搜索区域，热门区域，日期区域

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 var reallyDocumentTitle = document.title
 document.addEventListener("visibilitychange", function() {
-	document.title = document.hidden ? "你妹的，死回来！" : reallyDocumentTitle;
+	document.title = document.hidden ? "你妹的，快回来！" : reallyDocumentTitle;
 });
 window.onscroll=function(){
 	if($(window).scrollTop() > 80) {
@@ -15,4 +15,23 @@ window.onscroll=function(){
 }
 $(document).ready(function(){
 	$(".yx-rotaion").yx_rotaion({auto:true});
+	var mySchedule = new Schedule({
+		el: '#schedule-box',
+		//date: '2018-9-20',
+		clickCb: function (y,m,d) {
+//			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		nextMonthCb: function (y,m,d) {
+//			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		nextYeayCb: function (y,m,d) {
+//			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		prevMonthCb: function (y,m,d) {
+//			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		},
+		prevYearCb: function (y,m,d) {
+//			document.querySelector('#h3Ele').innerHTML = '日期：'+y+'-'+m+'-'+d	
+		}
+	});
 });
