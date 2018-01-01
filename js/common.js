6c5a7929967dff715fb043f164a3eeccc74aa4f9
yzw <yzw40459@ly.com>
2018-01-02 07:49:52 +0800

    --文章详情页面

--- js/common.js
+++ js/common.js
@@ -35,3 +35,44 @@ $(document).ready(function(){
 		}
 	});
 });
+var dic = new Array();
+function test(tnum){
+	if(dic.hasOwnProperty(tnum)){
+		layer.close(dic[tnum]);
+		layerui_article(tnum);
+	}else{
+		layerui_article(tnum);
+	}
+}
+
+function layerui_article(tnum){
+	layer.open({
+        type: 2 //此处以iframe举例
+        ,title: '<i class="fa fa-paperclip"></i>  测试一下  很多很多'
+        ,area: [$(window).width()/2+'px', $(window).height()+'px']
+        ,shade: 0
+        ,maxmin: true
+//      ,offset: [ //为了演示，随机坐标
+//        Math.random()*($(window).height()-300)
+//        ,Math.random()*($(window).width()-390)
+//      ] 
+        ,content: 'http://blog.csdn.net/wangdaoyin2010/article/details/70840680'
+        ,end:function(){
+        		delete dic[tnum]
+        }
+        
+//      ,btn: ['继续弹出', '全部关闭'] //只是为了演示
+//      ,yes: function(){
+//        $(that).click(); 
+//      }
+//      ,btn2: function(){
+//        layer.closeAll();
+//      }
+//      
+        ,zIndex: layer.zIndex //重点1
+        ,success: function(layero,index){
+      		dic[tnum]=index;
+          layer.setTop(layero); //重点2
+        }
+      });
+}
